99a3ad6460dbed05c4efa5a225ed913bca3a8cf7
Dardo-sys <54085965+Dardo-sys@users.noreply.github.com>
2019-09-04 21:27:47 -0300

    Add files via upload

--- action_page.php
+++ action_page.php
@@ -1,9 +1,22 @@
 <?php
 //var_dump($_GET)
 	echo "Su nombre es ".$_GET["nombre"];
+	
+	//fwrite($archivo, $_GET["nombre"]."\n");
+	//var_dump(json_encode($MiObjeto));
+	$MiObjeto = new stdClass();
+	$MiObjeto ->nombre = "nombre";
+	$MiObjeto ->apellido = "apellido";
+
 	$archivo=fopen("usuarios.txt","a");
-	fwrite($archivo, $_GET["nombre"]."\n"); 
-		# code...
+	fwrite($archivo, json_encode($MiObjeto));
+	fclose($archivo);
+	
+	
+
+	
+
+	//var_dump($MiObjeto);
 	
 
 
